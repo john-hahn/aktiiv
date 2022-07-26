67c785b906c3087fcf1dce588308775146e42120
John Hahn <johnjhhahn@gmail.com>
2022-07-26 11:20:29 -0500

    Added searchbar functionality - filters out results

--- program.js
+++ program.js
@@ -1,13 +1,16 @@
 const app = document.getElementById('root')
 
-const searchbar = document.createElement('input')
-searchbar.setAttribute('type', 'text')
+let input = document.getElementById('input')
+let button = document.getElementById('btn')
+
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
-
-app.appendChild(searchbar)
 app.appendChild(container)
+
+const lst = document.createElement('ol')
+lst.setAttribute('id', 'list')
+container.appendChild(lst)
 
 var request = new XMLHttpRequest()
 
@@ -17,18 +20,19 @@ request.onload = function () {
     exercises = data.results
     if (request.status >= 200 && request.status < 400) {
         exercises.forEach(exercise => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
+            const card = document.createElement('li')
+            card.setAttribute('class', 'exercise')
+
+            const cardBody = document.createElement('div')
+            cardBody.setAttribute('class', 'description')
 
             const h1 = document.createElement('h1')
             h1.textContent = exercise.name
 
-            const p = document.createElement('p')
-            p.textContent = `${exercise.description}...`
-
-            container.appendChild(card)
+            lst.appendChild(card)
             card.appendChild(h1)
-            card.appendChild(p)
+            card.appendChild(cardBody)
+            cardBody.innerHTML = `${exercise.description}`
         })
     } else {
         console.log('error')
@@ -36,3 +40,17 @@ request.onload = function () {
 }
 
 request.send()
+
+function search_exercise() {
+    var exerciseName = input.value
+    var $lis = $('ol li');
+    
+    for (i = 0; i < $lis.length; i++) { 
+        if (!$('ol li:eq(' + i + ')').text().includes(exerciseName)) {
+            $('ol li:eq(' + i + ')')[0].style.display="none";
+        }
+        else {
+            $('ol li:eq(' + i + ')')[0].style.display="list-item";
+        }
+    }
+}
