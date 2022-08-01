87e37cfea228774e573310c700ff52020b673841
John Hahn <johnjhhahn@gmail.com>
2022-07-31 23:35:07 -0500

    Days function dynamically

--- calendar.js
+++ calendar.js
@@ -1,5 +1,19 @@
 const date = new Date()
 
+date.setDate(1)
+
+const monthDays = document.querySelector('.days')
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+
+const firstDayIndex = date.getDay()
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay()
+
+const nextDays = 7 - lastDayIndex - 1
+
 const months = [
     'January',
     'February',
@@ -18,3 +32,23 @@ const months = [
 document.querySelector('.date h1').innerHTML = months[date.getMonth()]
 
 document.querySelector('.date p').innerHTML = date.toDateString()
+
+let days = ''
+
+for (let i = firstDayIndex; i > 0; i--) {
+    days += `<div class="prev-date">${prevLastDay - i + 1}</div>`
+}
+
+for (let j = 1; j <= lastDay; j++) {
+    if (j === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today">${j}</div>`
+    }
+    else {
+        days += `<div>${j}</div>`
+    }
+}
+
+for (let k = 1; k <= nextDays; k++) {
+    days += `<div class="next-date">${k}</div>`
+    monthDays.innerHTML = days
+}
