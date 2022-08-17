d9175d0c46c8beaf2a1ddbef331e5f20efba4a28
John Hahn <johnjhhahn@gmail.com>
2022-08-17 11:55:41 -0400

    Able to write from homepage to MongoDB

--- server.js
+++ server.js
@@ -1,25 +1,66 @@
+// Database connections
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/workoutdb"
+
 // Load Node modules
 var express = require('express');
 const ejs = require('ejs');
-// Initialise Express
+const bodyParser = require('body-parser');
 var app = express();
-// Render static files
+
+// Initializing Express
 app.use(express.static('public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
-// Port website will run on
-app.listen(8080);
+
+// Body-parser config
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Connecting variable db to database
+var db;
+MongoClient.connect(url, function(err, client) {
+    if (err) throw err;
+    db = client.db('workoutdb');
+    app.listen(8080);
+    console.log('Listening on 8080');
+});
 
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function (req, res) {
     res.render('pages/home');
 });
-
+// Program Route
 app.get('/program', function (req, res) {
     res.render('pages/program');
 });
-
+// Calendar Route
 app.get('/calendar', function (req, res) {
     res.render('pages/calendar');
 });
+
+// *** POST Routes ***
+// Add Route
+app.post('/add', function(req, res) {
+    // Get details from the form
+    var chestWorkout = req.body.chest;
+    var backWorkout = req.body.back;
+    var shouldersWorkout = req.body.shoulders;
+    var bicepsWorkout = req.body.biceps;
+    var tricepsWorkout = req.body.triceps;
+    // Format workout details into JSON
+    var workoutDays = {"chest": chestWorkout, 
+                       "back": backWorkout, 
+                       "shoulders": shouldersWorkout, 
+                       "biceps": bicepsWorkout, 
+                       "triceps": tricepsWorkout}
+    // Add workout details to workout collection
+    db.collection('workouts').insertOne(workoutDays, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.redirect('/');
+    })
+})
